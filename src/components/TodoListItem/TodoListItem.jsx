455f98331a2bdb5073e9e497540f293eaeb56f57
JuliaPriadka <150530521+JuliaPriadka@users.noreply.github.com>
2024-05-26 12:57:38 +0300

    Add delete & add todo

--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -2,14 +2,18 @@ import style from './TodoListItem.module.css';
 import { Text } from 'components';
 import { RiDeleteBinLine } from 'react-icons/ri';
 
-export const TodoListItem = ({ text, count }) => {
+export const TodoListItem = ({ text, count, onDelete, id }) => {
   return (
     <div className={style.box}>
       <Text textAlign="center" marginBottom="20">
         TODO #{count}
       </Text>
       <Text>{text}</Text>
-      <button className={style.deleteButton} type="button">
+      <button
+        className={style.deleteButton}
+        type="button"
+        onClick={() => onDelete(id)}
+      >
         <RiDeleteBinLine size={24} />
       </button>
     </div>
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,20 +1,40 @@
 import { Text, Form, TodoList } from 'components';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 
 export const Todos = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(
+    () => JSON.parse(localStorage.getItem('saved-todos')) || [],
+  );
+
+  useEffect(() => {
+    localStorage.setItem('saved-todos', JSON.stringify(todos));
+  }, [todos]);
 
   const onSubmit = text => {
+    const isExist = todos.find(
+      todo => todo.text.toLowerCase() === text.toLowerCase(),
+    );
+    if (isExist) {
+      alert('todo already exist');
+      return;
+    }
     const todo = { text, id: nanoid() };
     setTodos(prevState => [...prevState, todo]);
+  };
+
+  const onDelete = id => {
+    setTodos(prevState => prevState.filter(todo => todo.id !== id));
   };
 
   return (
     <>
       <Form onSubmit={onSubmit} />
-      <TodoList todos={todos} />
-      <Text textAlign="center">There are no any todos ...</Text>
+      {todos.length ? (
+        <TodoList todos={todos} onDelete={onDelete} />
+      ) : (
+        <Text textAlign="center">There are no any todos ...</Text>
+      )}
     </>
   );
 };
